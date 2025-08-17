a3ac22515280cbc6bd9d266555cd9f92d0c85dae
spandanc201 <mailatspandan@gmail.com>
2025-08-17 10:38:57 -0400

    Fix for Date Data Type
    Had to fix data for dates that were being processed as written figures instead of actual dates.

--- dataWrangling.py
+++ dataWrangling.py
@@ -66,7 +66,9 @@ ORDER BY CATEGORY, total_sales DESC;
 # -------- Execute queries and save CSVs --------
 pd.read_sql_query(query1, conn).to_csv('new_orders_by_region.csv', index=False)
 pd.read_sql_query(query2, conn).to_csv('total_sales_by_category.csv', index=False)
-pd.read_sql_query(query3, conn).to_csv('total_sales_on_each_date.csv', index=False)
+df_date = pd.read_sql_query(query3, conn)
+df_date['order_date'] = pd.to_datetime(df_date['order_date'], format='%Y-%m-%d')
+df_date.to_csv('total_sales_on_each_date.csv', index=False)
 pd.read_sql_query(query4, conn).to_csv('popular_products.csv', index=False)
 pd.read_sql_query(query5, conn).to_csv('total_sales_by_category_and_subcategory.csv', index=False)
 
